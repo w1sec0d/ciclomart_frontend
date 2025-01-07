e6b1a98432d9057b3c52e1f1826eb6a368f4e01b
Carlos Ramírez <cramirezmun@unal.edu.co>
2025-01-06 21:58:59 -0500

    Add basic style register page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,22 +6,22 @@ const Layout = () => {
   return (
     <div>
       <header>
-        <h1>Register</h1>
-        <nav className="bg-primary p-4">
+        {/* <h1>Register</h1> */}
+        <nav className="bg-primary p-4 font-medium shadow-md md:h-[64px] flex items-center fixed left-0 top-0 w-full z-10">
           <ul className="flex flex-col md:flex-row">
-            <li>
+            <li className="mx-4 hover:font-bold">
               <a href="/">Home</a>
             </li>
-            <li>
+            <li className="mx-4 hover:font-bold">
               <a href="/about">About</a>
             </li>
-            <li>
+            <li className="mx-4 hover:font-bold">
               <a href="/contact">Contact</a>
             </li>
           </ul>
         </nav>
       </header>
-      <main>
+      <main className="mt-[64px]">
         <Outlet />
       </main>
       <footer></footer>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -10,8 +10,11 @@ const Register = () => {
 
   return (
     <section className="flex flex-col md:flex-row">
-      <div>
-        <form onSubmit={handleSubmit}>
+      <div className="bg-white md:w-3/5 p-8">
+        {/* form paragaph */}
+        <h1 className="font-black text-4xl">Únete a CicloMart</h1>
+        <p>Crea una cuenta gratuita o inicia sesión</p>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
           <label htmlFor="email">Correo electrónico</label>
           <input type="email" id="email" />
           <div>
@@ -26,10 +29,10 @@ const Register = () => {
           <input type="password" id="password-confirm" />
           <input type="checkbox" id="terms" />
           <label htmlFor="terms">Acepto los términos y condiciones</label>
-          <button type="submit">Registrarse</button>
+          <button type="submit">Continuar</button>
         </form>
       </div>
-      <div>
+      <div className="bg-primary md:w-2/5">
         <img
           src={registerImage}
           alt="Imagen de una persona sonriendo en una bicicleta y saludando"
