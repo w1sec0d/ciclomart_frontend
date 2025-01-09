4afa36a8d7b709007035c78179d148b11a594e69
JuMad-SE <jumadridc@unal.edu.co>
2025-01-09 17:57:01 -0500

    Initializes profile view with navigation and route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router'
 import Landing from './pages/Landing'
 import Layout from './components/Layout'
 import Register from './pages/Register'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Landing />} />
         <Route path="register" element={<Register />} />
+        <Route path="profile" element={<Profile />} />
       </Route>
     </Routes>
   )
