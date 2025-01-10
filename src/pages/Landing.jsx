92aa2905a578d3a3c6b0b91de10c201488e12011
Carlos Ramírez <cramirezmun@unal.edu.co>
2025-01-10 13:54:07 -0500

    Add basic item carousel

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,10 +2,11 @@ import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css'
 
 import landing1 from '../assets/landing1.webp'
-import Button from '../components/Button'
+import ItemContainer from '../components/ItemContainer'
+import bike1 from '../assets/bike1.webp'
 
 const LandingPage = () => {
-  const responsive = {
+  const landingCarousel = {
     // superLargeDesktop: {
     //   // the naming can be any, depends on you.
     //   breakpoint: { max: 4000, min: 3000 },
@@ -24,6 +25,25 @@ const LandingPage = () => {
       items: 1,
     },
   }
+  const itemContainer = {
+    // superLargeDesktop: {
+    //   // the naming can be any, depends on you.
+    //   breakpoint: { max: 4000, min: 3000 },
+    //   items: 5,
+    // },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 5,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 5,
+    },
+  }
   return (
     <section>
       <h1 className="w-full text-center text-4xl py-10">
@@ -32,7 +52,7 @@ const LandingPage = () => {
         <span className="font-bold">mercado bici </span>
         de <span className="font-bold">confianza</span>
       </h1>
-      <Carousel responsive={responsive}>
+      <Carousel responsive={landingCarousel}>
         <div className="flex items-center justify-center w-full relative bg-[#ebf9f6]">
           <img
             src={landing1}
@@ -41,6 +61,51 @@ const LandingPage = () => {
             style={{ boxShadow: '0 0 8px 8px #ebf9f6 inset' }}
           />
         </div>
+      </Carousel>
+      <h2 className="text-3xl text-center font-bold my-10">Lo más vendido</h2>
+      <Carousel responsive={itemContainer}>
+        <ItemContainer
+          name="Bicicleta de carbono S-Works Ruta"
+          img={bike1}
+          fullPrice={3000000}
+          price={2250000}
+        />
+        <ItemContainer
+          name="Bicicleta de carbono S-Works Ruta"
+          img={bike1}
+          fullPrice={3000000}
+          price={2250000}
+        />
+        <ItemContainer
+          name="Bicicleta de carbono S-Works Ruta"
+          img={bike1}
+          fullPrice={3000000}
+          price={2250000}
+        />
+        <ItemContainer
+          name="Bicicleta de carbono S-Works Ruta"
+          img={bike1}
+          fullPrice={3000000}
+          price={2250000}
+        />
+        <ItemContainer
+          name="Bicicleta de carbono S-Works Ruta"
+          img={bike1}
+          fullPrice={3000000}
+          price={2250000}
+        />
+        <ItemContainer
+          name="Bicicleta de carbono S-Works Ruta"
+          img={bike1}
+          fullPrice={3000000}
+          price={2250000}
+        />
+        <ItemContainer
+          name="Bicicleta de carbono S-Works Ruta"
+          img={bike1}
+          fullPrice={3000000}
+          price={2250000}
+        />
       </Carousel>
     </section>
   )
