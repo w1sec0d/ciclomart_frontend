14a99a400a9a2dfaab1e2db95ef9fea12dc2f92d
Carlos Ramírez <cramirezmun@unal.edu.co>
2025-01-14 15:49:29 -0500

    Update Navbar and Landing page with new content and styling enhancements

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,7 +5,14 @@ import 'react-multi-carousel/lib/styles.css'
 // Assets importing
 import landing1 from '../assets/landing1.webp'
 import bike1 from '../assets/bike1.webp'
-import { LocalFireDepartment } from '@mui/icons-material'
+import bike2 from '../assets/bike2.webp'
+import offer from '../assets/offer.png'
+import repuestos from '../assets/repuestos.webp'
+import {
+  LocalFireDepartment,
+  PedalBike,
+  SettingsSuggest,
+} from '@mui/icons-material'
 
 // Components
 import ItemContainer from '../components/ItemContainer'
@@ -51,17 +58,11 @@ const LandingPage = () => {
   }
   return (
     <section>
-      {/* <h1 className="w-full text-center text-4xl py-10">
-        ¡Bienvenido a <span className="text-tertiary font-bold">Ciclo</span>
-        <span className="text-secondary font-bold">Mart</span>! Tu{' '}
-        <span className="font-bold">mercado bici </span>
-        de <span className="font-bold">confianza</span>
-      </h1> */}
       <Carousel responsive={landingCarousel}>
         <div className="flex items-center justify-center w-full relative bg-[#ebf9f6]">
           <img
             src={landing1}
-            alt="Imagen de una persona sobre una bicicleta mirando un paisaje boscoso"
+            alt="Imagen de una bicicleta deportiva blanca con el mensaje: Encuentra tu nueva bici con los componentes que necesitas (has click aqui)"
             className="max-h-[80vh] object-contain"
             style={{ boxShadow: '0 0 8px 8px #ebf9f6 inset' }}
           />
@@ -115,6 +116,70 @@ const LandingPage = () => {
           price={2250000}
         />
       </Carousel>
+      <h2 className="text-3xl text-center font-bold my-10">
+        <PedalBike fontSize="large" /> Explora tu mundo bici
+      </h2>
+      <section className="flex flex-row justify-between max-w-[800px] mx-auto">
+        <div>
+          <img src={offer} className="w-[200px] h-[200px] object-contain" />
+          <h3 className="font-semibold text-center my-2 text-xl">Ofertas</h3>
+        </div>
+        <div>
+          <img src={bike2} className="w-[200px] h-[200px] object-contain" />
+          <h3 className="font-semibold text-center my-2 text-xl">Bicicletas</h3>
+        </div>
+        <div>
+          <img src={repuestos} className="w-[200px] h-[200px] object-contain" />
+          <h3 className="font-semibold text-center my-2 text-xl">Repuestos</h3>
+        </div>
+      </section>
+      <section className="my-6">
+        <h2 className="text-3xl text-center font-bold my-10">
+          <SettingsSuggest fontSize="large" /> Encuentra{' '}
+          <span className="italic">ese repuesto</span> que necesitas
+        </h2>
+        <Carousel responsive={itemContainer} className="ml-7 mb-10">
+          <ItemContainer
+            name="Repuesto"
+            img={repuestos}
+            fullPrice={3000000}
+            price={2250000}
+            freeShipping={true}
+          />
+          <ItemContainer
+            name="Repuesto"
+            img={repuestos}
+            fullPrice={3000000}
+            price={2250000}
+            freeShipping={true}
+          />
+          <ItemContainer
+            name="Repuesto"
+            img={repuestos}
+            fullPrice={3000000}
+            price={2250000}
+          />
+          <ItemContainer
+            name="Repuesto"
+            img={repuestos}
+            fullPrice={3000000}
+            price={2250000}
+          />
+          <ItemContainer
+            name="Repuesto"
+            img={repuestos}
+            fullPrice={3000000}
+            price={2250000}
+          />
+          <ItemContainer
+            name="Repuesto"
+            img={repuestos}
+            fullPrice={3000000}
+            price={2250000}
+            freeShipping={true}
+          />
+        </Carousel>
+      </section>
     </section>
   )
 }
