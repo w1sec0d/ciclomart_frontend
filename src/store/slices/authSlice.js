eba07751842c63c9d8b6a48f8acd6a55e1cd7c0d
Carlos Ramírez <cramirezmun@unal.edu.co>
2025-01-15 15:23:30 -0500

    Quick fix get user 2

--- src/store/slices/authSlice.js
+++ src/store/slices/authSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
-import getUserFromLocalStorage from '../../utils/GetUser'
+import getUserFromLocalStorage from '../../utils/getUser'
 
 const initialState = {
   authUser: getUserFromLocalStorage(),
