4a8cb7aa21757581d6f879d1f7bbc603cafb32ad
Carlos Ramírez <cramirezmun@unal.edu.co>
2025-01-15 10:15:26 -0500

    Improve form error handling

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -2,26 +2,23 @@ import Input from './Input'
 import { useForm } from 'react-hook-form'
 import { useDispatch } from 'react-redux'
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
 import { setNotification } from '../store/slices/notificationSlice'
-import apiService from '../services/apiService'
 import Checkbox from './Checkbox'
 import Button from './Button'
 import loginService from '../services/loginService'
 
 const RegisterForm = () => {
-  const navigate = useNavigate()
-
-  const [state, setState] = useState({
-    errors: {},
-  })
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/
 
   const dispatch = useDispatch()
-  const { register, handleSubmit, reset } = useForm()
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm()
 
   const sendEmail = async (values) => {
-    //const email = values.email
-
     const request = await loginService.sendCodeRegister(values)
     if (request.status === 200) {
       return true
@@ -38,86 +35,36 @@ const RegisterForm = () => {
     return false
   }
 
-  const validation = (values) => {
-    let error = {}
-    error.email = ''
-    error.password = ''
-    error.invalid = ''
-
-    if (values.password != values['password-confirm']) {
-      error.invalid = 'Las contraseñas no coinciden'
-    }
-
-    // Expresión regular para validar el correo electrónico
-    let emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/
-    if (!emailRegex.test(values.email)) {
-      error.email = 'Correo electronico no valido'
-    }
-
-    //La contraseña debe tener al menos 8 caracteres, una letra mayuscula, una letra minuscula y un número
-    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/
-    if (!passwordRegex.test(values.password)) {
-      error.password =
-        'La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula y un número.'
-    }
-
-    return error
-  }
-
+  console.log('errors', errors)
   const onSubmit = async (data) => {
-    const errors = validation(data)
-
-    setState({
-      ...state,
-      errors: errors,
-    })
-
-    if (
-      errors.email === '' &&
-      errors.password === '' &&
-      errors.invalid === ''
-    ) {
-      const validateEmail = await sendEmail(data)
-      const verifirEmail = await verifyEmail(data)
-
-      if (verifirEmail) {
-        dispatch(
-          setNotification({
-            title: '¡Ya existe un usuairo!',
-            text: 'Ya existe un usuario con este correo.',
-            icon: 'error',
-          })
-        )
-      } else {
-        if (validateEmail) {
-          alert('Hola te enviamos un código para terminar el registro.')
-          dispatch(
-            setNotification({
-              title: '¡Éxito!',
-              text: 'Hola te enviamos un código para terminar el registro.',
-              icon: 'success',
-            })
-          )
-          reset()
-        }
-      }
-
-      /*const request = await apiService.createUsuario(data)
-      if (request) {
-        dispatch(
-          setNotification({
-            title: 'Usuario creado',
-            text: `El usuario ha sido creado`,
-            icon: 'success',
-          })
-        )
-
-        setTimeout(() => {
-          navigate('/login')
-        }, 2000)
-      }*/
-    }
-    // Clear form
+    // if (
+    //   errors.email === '' &&
+    //   errors.password === '' &&
+    //   errors.invalid === ''
+    // ) {
+    //   const validateEmail = await sendEmail(data)
+    //   const verifirEmail = await verifyEmail(data)
+    //   if (verifirEmail) {
+    //     dispatch(
+    //       setNotification({
+    //         title: '¡Ya existe un usuairo!',
+    //         text: 'Ya existe un usuario con este correo.',
+    //         icon: 'error',
+    //       })
+    //     )
+    //   } else {
+    //     if (validateEmail) {
+    //       dispatch(
+    //         setNotification({
+    //           title: '¡Éxito!',
+    //           text: 'Hola te enviamos un código para terminar el registro.',
+    //           icon: 'success',
+    //         })
+    //       )
+    //       reset() // Clear form
+    //     }
+    //   }
+    // }
   }
   return (
     <>
@@ -130,8 +77,8 @@ const RegisterForm = () => {
           type="email"
           {...register('email', { required: true })}
         />
-        {state.errors.email && (
-          <span className="text-red-500 text-xs">{state.errors.email}</span>
+        {errors.email && (
+          <span className="text-red-500 text-xs">{errors.email.message}</span>
         )}
         <div className="flex gap-4">
           <Input
@@ -151,19 +98,37 @@ const RegisterForm = () => {
           id="password"
           label="Contraseña"
           type="password"
-          {...register('password', { required: true })}
+          {...register('password', {
+            required: 'Password is required',
+            pattern: {
+              value: passwordRegex,
+              message:
+                'La contraseña debe tener al menos 6 caracteres, una letra mayúscula, una letra minúscula y un número.',
+            },
+          })}
         />
-        {state.errors.password && (
-          <span className="text-red-500 text-xs">{state.errors.password}</span>
+        {errors.password && (
+          <span className="text-red-500 text-xs">
+            {errors.password.message}
+          </span>
         )}
         <Input
           id="password-confirm"
           label="Confirmar contraseña"
           type="password"
-          {...register('password-confirm', { required: true })}
+          {...register('passwordConfirm', {
+            required: 'Password is required',
+            pattern: {
+              value: passwordRegex,
+              message:
+                'La contraseña debe tener al menos 6 caracteres, una letra mayúscula, una letra minúscula y un número.',
+            },
+          })}
         />
-        {state.errors.invalid && (
-          <span className="text-red-500 text-xs">{state.errors.invalid}</span>
+        {errors.passwordConfirm && (
+          <span className="text-red-500 text-xs">
+            {errors.passwordConfirm.message}
+          </span>
         )}
         <div>
           <Checkbox id="terms" {...register('terms', { required: true })}>
--- src/utils/getUser.js
+++ src/utils/getUser.js
@@ -2,6 +2,8 @@ import userInfoService from '../services/userInfoService'
 
 const getUserFromLocalStorage = async () => {
   const token = window.localStorage.getItem('token')
+  if (!token) return null
+
   const response = await userInfoService.getUserInfo(token)
   return response.user ? response.user : null
 }
