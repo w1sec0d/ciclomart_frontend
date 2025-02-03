4ab5bc34e0842fbc2d1695348a3f385231bf9a30
Carlos Ramírez <cramirezmun@unal.edu.co>
2025-02-03 12:25:45 -0500

    Agrega export api service

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,9 @@
+import axios from 'axios'
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001'
 
 const getProducts = async () => {
   const request = await axios.get(API_URL + '/products')
   return request.data
 }
+
+export default { getProducts }
