d5a420bf61e6f4ef5c909169fe45d85256a2dcd0
Carlos Ramírez <cramirezmun@unal.edu.co>
2025-02-05 10:32:58 -0500

    Agrega formateo con Prettier

--- src/services/mercadoPago.js
+++ src/services/mercadoPago.js
@@ -2,13 +2,13 @@ import axios from 'axios'
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001'
 
 const createPreference = async (item) => {
-    try {
-        const response = await axios.post(API_URL + '/createPreference', item)
-        const { id } = response.data
-        return id
-    } catch (error) {
-        console.error('Error creando la preferencia de Mercado Pago:', error)
-    }
+  try {
+    const response = await axios.post(API_URL + '/createPreference', item)
+    const { id } = response.data
+    return id
+  } catch (error) {
+    console.error('Error creando la preferencia de Mercado Pago:', error)
+  }
 }
 
 export default { createPreference }
