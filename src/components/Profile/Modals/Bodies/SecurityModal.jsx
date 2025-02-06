2d0355f10d00e46d3060204944964c53ea6b67c2
JuMad-SE <jumadridc@unal.edu.co>
2025-02-05 20:32:31 -0500

    Mejora la funcionalidad de cerrar sesión y la hace reutilizable como util

--- src/components/Profile/Modals/Bodies/SecurityModal.jsx
+++ src/components/Profile/Modals/Bodies/SecurityModal.jsx
@@ -7,23 +7,15 @@ import ChangeEmail from '@mui/icons-material/EmailOutlined'
 import LogOut from '@mui/icons-material/ExitToAppOutlined'
 
 //-> Utilidades
+import logOut from '../../../../utils/logOut'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
-import { cleanShowModal } from '../../../../store/slices/showModalSlice'
-import { clearAuth } from '../../../../store/slices/authSlice'
+import { useSelector } from 'react-redux'
 
 const SecurityModal = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch()
-
-  const handleLogOut = () => {
-    localStorage.removeItem('token')
-    dispatch(clearAuth())
-    dispatch(cleanShowModal())
-    document.body.style.overflow = 'auto'
-    navigate('/')
-  }
-
+  const activeModal = useSelector((state) => state.showModal.activeModal)
   return (
     <div className="grid grid-cols-1 gap-0 h-full">
       <OptionSelector text={'Cambia tu contraseña'}>
@@ -41,7 +33,7 @@ const SecurityModal = () => {
       <OptionSelector
         text={'Cerrar sesión'}
         className={'border-0 hover:border-red-400  hover:bg-red-200'}
-        onClick={handleLogOut}
+        onClick={() => logOut(dispatch, navigate, activeModal)}
       >
         <LogOut
           className="opacity-30 ml-4 text-red-950"
