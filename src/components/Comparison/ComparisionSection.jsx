a782b06bd44fe0d9a8aded3c8a3fb229cb3e0032
JuMad-SE <jumadridc@unal.edu.co>
2025-02-07 20:34:55 -0500

    Termina disposición inicial de las secciones de la vista de comparación

--- src/components/Comparison/ComparisionSection.jsx
+++ src/components/Comparison/ComparisionSection.jsx
@@ -1,12 +1,24 @@
-const ComparisionSection = ({ title }) => {
+import { twMerge } from 'tailwind-merge'
+
+const ComparisionSection = ({ title, className1, className2, className3 }) => {
   return (
     <div>
-      <div className="h-20 border-y border-y-lgray w-full">
+      <div
+        className={twMerge(
+          'text-zinc-500 h-10 border-y border-y-lgray w-full flex items-center justify-center shadow-xl text-xl',
+          className3
+        )}
+      >
         <b>{title}</b>
       </div>
       <div className="h-72 flex flex-row w-full">
-        <div className="w-1/2 bg-white h-full  border-r border-r-lgray rounded-tl-3xl  rounded-bl-3xl"></div>
-        <div className="w-1/2 h-full bg-white rounded-tr-3xl rounded-br-3xl "></div>
+        <div
+          className={twMerge(
+            'w-1/2 bg-white h-full  border-r border-r-lgray',
+            className1
+          )}
+        ></div>
+        <div className={twMerge('w-1/2 h-full bg-white ', className2)}></div>
       </div>
     </div>
   )
--- src/pages/ComparisonView.jsx
+++ src/pages/ComparisonView.jsx
@@ -1,9 +1,13 @@
+// Componentes
 import BuyButton from '../components/Comparison/BuyButton'
 import ComparisionSection from '../components/Comparison/ComparisionSection'
 
+// Iconos
+import SouthIcon from '@mui/icons-material/South'
+
 const Header = () => {
   return (
-    <div className="h-[50px] rounded-t-3xl flex flex-row relative">
+    <div className="h-[50px] flex flex-row relative">
       <BuyButton>4.000.000</BuyButton>
       <div className="z-10 h-5/6 bg-lblue w-full absolute top-0 left-0 rounded-t-3xl flex justify-center items-center">
         <b className="text-xl mx-4">Comparación</b>
@@ -22,17 +26,37 @@ const Header = () => {
 const ComparisonView = () => {
   return (
     <div className="h-auto flex flex-col">
-      <div className="h-auto w-auto m-10 rounded-3xl bg-white drop-shadow-lg flex flex-col">
+      <div className="h-auto w-auto mx-10 mb-8 my-10 rounded-3xl bg-white drop-shadow-lg flex flex-col">
         {/*Header */}
         <Header />
         {/*Sección nombre y foto producto 1 y 2 respectivamente*/}
         <div className="h-72 flex flex-row w-full">
-          <div className="w-1/2 bg-white h-full  border-r border-r-lgray rounded-tl-3xl  "></div>
-          <div className="w-1/2 h-full bg-white rounded-tr-3xl "> </div>
+          <div className="w-1/2 bg-white h-full  border-r border-r-lgray   "></div>
+          <div className="w-1/2 h-full bg-white  "> </div>
         </div>
         {/*Sección elementos compartidos producto 1 y 2 respectviamente */}
         <ComparisionSection title={'Elementos Compartidos'} />
-        <ComparisionSection title={'Agregados'} />
+        {/*Sección elementos Agregados */}
+        <ComparisionSection
+          title={'Agregados'}
+          className1={'rounded-bl-3xl'}
+          className2={'rounded-br-3xl'}
+        />
+      </div>
+      <button className="mx-10 mb-10 bg-tertiary bg-opacity-45 rounded-xl h-12 drop-shadow-2xl hover:bg-tertiary">
+        <b className="text-xl">Resaltar Diferencias</b>
+      </button>
+
+      <div className="flex justify-center">
+        <SouthIcon style={{ fontSize: '9rem', opacity: 0.1 }}></SouthIcon>
+      </div>
+      <div className="h-auto w-auto mx-10 mb-8 my-10 rounded-bl-3xl rounded-br-3xl bg-white drop-shadow-lg flex flex-col">
+        <ComparisionSection
+          title={'Información del vendedor'}
+          className1={'rounded-bl-3xl'}
+          className2={'rounded-br-3xl'}
+          className3={'bg-primary rounded-t-3xl h-14 text-black'}
+        />
       </div>
     </div>
   )
