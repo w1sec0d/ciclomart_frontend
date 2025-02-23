22ac13b6a083f07ac75a9fa9aee63e3d9d24fe62
JuMad-SE <jumadridc@unal.edu.co>
2025-02-22 22:30:53 -0500

    Mejora el diseño de la selección del elemento a publicar

--- src/components/Publish/ProductSelection.jsx
+++ src/components/Publish/ProductSelection.jsx
@@ -5,33 +5,39 @@ import sparePartIcon from '../../assets/sparePartIcon.png'
 
 const ProductSelect = ({ onSelect }) => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-2xl font-bold mb-8">¿Qué quieres publicar?</h1>
-      <div className="flex space-x-4">
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-t from-primary/95 to-zinc-100 from-50% to-50% relative">
+      <h1 className="text-3xl font-bold mb-9 absolute top-20">
+        ¿Qué quieres publicar?
+      </h1>
+      <div className="flex space-x-24 items-center justify-center">
         {' '}
         {/* Flex container for buttons */}
-        <Button
-          onClick={() => onSelect('bicicleta')}
-          className="flex items-center justify-center bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600"
-        >
-          <img
-            src={bikeIcon} // Bicycle icon
-            alt="Bicycle"
-            className="w-6 h-6 mr-2"
-          />
-          Bicicleta
-        </Button>
-        <Button
-          onClick={() => onSelect('componente')}
-          className="flex items-center justify-center bg-green-500 text-white px-6 py-2 rounded-lg hover:bg-green-600"
-        >
-          <img
-            src={sparePartIcon} // Spare part icon
-            alt="Spare Part"
-            className="w-6 h-6 mr-2"
-          />
-          Repuesto
-        </Button>
+        <div className="size-40 ">
+          <Button
+            onClick={() => onSelect('bicicleta')}
+            className="flex flex-col items-center justify-center bg-white text-black font-bold shadow-xl px-6 py-2 rounded-lg hover:bg-dgray h-full w-full"
+          >
+            <img
+              src={bikeIcon} // Bicycle icon
+              alt="Bicycle"
+              className="w-full h-full  "
+            />
+            Bicicleta
+          </Button>
+        </div>
+        <div className="size-40 ">
+          <Button
+            onClick={() => onSelect('bicicleta')}
+            className="flex flex-col items-center justify-center bg-white text-black font-bold shadow-xl px-6 py-2 rounded-lg hover:bg-dgray h-full w-full"
+          >
+            <img
+              src={sparePartIcon} // Bicycle icon
+              alt="Bicycle"
+              className="h-full w-full mb-4"
+            />
+            Repuesto
+          </Button>
+        </div>
       </div>
     </div>
   )
