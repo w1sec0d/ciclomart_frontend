9c2ce5691d2b08b8b104e05874841078e527c4f1
JuMad-SE <jumadridc@unal.edu.co>
2025-02-26 17:02:00 -0500

    Implementa paginación sencilla

--- src/pages/Bicicleta.jsx
+++ src/pages/Bicicleta.jsx
@@ -1,48 +1,83 @@
 //Componentes
 import ItemContainer from '../components/ItemContainer'
+import ComparisonBar from '../components/Comparison/ComparisonBar'
 
 //Utilidades
 import { useQuery } from 'react-query'
+import { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { setLoading, clearLoading } from '../store/slices/loadingSlice'
+import ReactPaginate from 'react-paginate'
 
 //Servicios
 import { getBicicletas, getProducts } from '../services/productService'
 
 const Bicicleta = () => {
   const dispatch = useDispatch()
+  const itemsPerPage = 5
+  const [currentPage, setCurrentPage] = useState(0)
+
+  //Trae bicicletas
   const {
     data: bicicletas,
     isError,
     isLoading,
   } = useQuery(['productos'], getProducts)
 
-  if (isLoading) {
-    dispatch(setLoading())
-    return
-  } else if (bicicletas) {
-    dispatch(clearLoading())
+  // Maneja cambio de página
+  const handlePageClick = ({ selected }) => {
+    setCurrentPage(selected)
   }
-  if (isError) return <p>Error: {isError.message}</p>
+
+  useEffect(() => {
+    if (isLoading) {
+      dispatch(setLoading())
+    } else {
+      dispatch(clearLoading())
+    }
+  }, [isLoading, dispatch])
+
+  if (isLoading) return null
+  if (isError) return <p>Error: {error.message}</p>
+  // Calcula que elementos mostrar
+
+  const offset = currentPage * itemsPerPage
+  const currentItems = bicicletas.slice(offset, offset + itemsPerPage)
+
+  console.log(currentItems)
 
   return (
-    <div className="bg-lgray pb-8">
+    <div className="bg-lgray pb-8 ">
+      <ComparisonBar />
       <div>
-        <h1 className="font-bold text-3xl bg-secondary w-full h-20 mb-10 shadow-xl flex items-center justify-center">
+        <h1 className="font-bold text-3xl bg-primary w-full h-20 mb-10 shadow-xl flex items-center justify-center">
           ¡Encuentra tu próxima bicicleta!
         </h1>
       </div>
-      <div className="px-9">
+      <div className="px-9 mb-4">
         <div className="grid grid-cols-5">
-          {bicicletas.map((bicicleta) => (
+          {currentItems.map((bicicleta) => (
             <ItemContainer
               {...bicicleta}
-              key={bicicleta.idBicicleta}
+              key={bicicleta.idProducto}
               className={'mt-2'}
               envioGratis={bicicleta['método de envio'] === 'gratis'}
             />
           ))}
         </div>
+      </div>
+      <div className="flex items-center justify-center">
+        <ReactPaginate
+          previousLabel={'← Anterior'}
+          nextLabel={'Siguiente →'}
+          breakLabel={'...'}
+          pageCount={Math.ceil(bicicletas.length / itemsPerPage)}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={3}
+          onPageChange={handlePageClick}
+          containerClassName={'pagination'}
+          activeClassName={'active'}
+        />
       </div>
     </div>
   )
