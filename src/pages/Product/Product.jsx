5fcf31868e4a4967eebce4b386c9f0f90ad01544
JuMad-SE <jumadridc@unal.edu.co>
2025-02-27 23:52:28 -0500

    Adecua espacio de reseñas y mejora la distribución de colores

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -174,158 +174,160 @@ const ProductPage = () => {
 
   return (
     <section className="px-10 bg-lgray h-full flex flex-col">
-      <div className=" bg-white px-6 my-4 rounded-xl  ">
-        <div className="flex justify-evenly items-center py-10">
-          {/* <Img
+      <div className=" bg-white  rounded-xl  my-4 ">
+        {/* <div className="w-full h-auto py-2 flex items-center justify-center rounded-t-xl bg-primary/5 shadow-xl ">
+          <h1 className="font-bold text-2xl">Información del producto</h1>
+        </div> */}
+        <div className="px-6">
+          <div className="flex justify-evenly items-center py-10">
+            {/* <Img
           src={producto.imagenURL}
           alt={'Imagen del producto: ' + producto.nombre}
           className="max-w-[500px] max-h-[500px]"
           /> */}
 
-          <div className="w-3/5 h-auto bg-wite shadow-xl border-primary border rounded-xl py-2 px-2 ">
-            <GalleryImages />
-          </div>
-          <div className="p-4 w-2/5 border rounded-xl  border-primary">
-            <div className="flex items-center">
-              <div>
-                <h1 className="text-2xl font-bold tracking-wide">
-                  {producto.nombre}
-                </h1>
-                <p className="text-2xl text-gray-700 tracking-wide">
-                  {colombianPrice(producto.precio)}
+            <div className="w-3/5 h-auto bg-wite shadow-xl border-primary border rounded-xl py-2 px-2 ">
+              <GalleryImages />
+            </div>
+            <div className="p-4 w-2/5 border rounded-xl  border-primary">
+              <div className="flex items-center">
+                <div>
+                  <h1 className="text-2xl font-bold tracking-wide">
+                    {producto.nombre}
+                  </h1>
+                  <p className="text-2xl text-gray-700 tracking-wide">
+                    {colombianPrice(producto.precio)}
+                  </p>
+                </div>
+                <FavoriteBorder
+                  sx={{ fontSize: 40, color: 'gray', marginLeft: 4 }}
+                />
+              </div>
+              {/* Detalles del producto */}
+              <div className="my-2 tracking-wide">
+                <p>
+                  <b>Marca</b>: {producto.nombreMarca ?? 'Genérica'}
+                </p>
+                <p>
+                  <b>Tipo</b>: {capitalize(producto.tipo)}
+                </p>
+                <p>
+                  <b>Estado</b>:{' '}
+                  {producto.disponibilidad === 'disponible'
+                    ? 'Disponible'
+                    : 'No Disponible'}
+                </p>
+                <p>
+                  <b>Publicado el: </b>
+                  {new Date(producto.fechaPublicacion).toLocaleDateString()}
                 </p>
               </div>
-              <FavoriteBorder
-                sx={{ fontSize: 40, color: 'gray', marginLeft: 4 }}
-              />
-            </div>
-            {/* Detalles del producto */}
-            <div className="my-2 tracking-wide">
-              <p>
-                <b>Marca</b>: {producto.nombreMarca ?? 'Genérica'}
-              </p>
-              <p>
-                <b>Tipo</b>: {capitalize(producto.tipo)}
-              </p>
-              <p>
-                <b>Estado</b>:{' '}
-                {producto.disponibilidad === 'disponible'
-                  ? 'Disponible'
-                  : 'No Disponible'}
-              </p>
-              <p>
-                <b>Publicado el: </b>
-                {new Date(producto.fechaPublicacion).toLocaleDateString()}
-              </p>
-            </div>
-            <div className="flex items-center flex-row">
-              <Input
-                type="number"
-                label="Cantidad"
-                id="cantidad"
-                name="cantidad"
-                value={cantidad}
-                onChange={(e) => setCantidad(Number(e.target.value))}
-                min="1"
-                max={producto.cantidad}
-                className="mt-1 my-4 block w-1/3"
-              />
-              <Button className="mx-3" onClick={handleAddToCart}>
-                🛒+{' '}
-              </Button>
-              <Button className="mx-3" onClick={handleBuy}>
-                Comprar
-              </Button>
-            </div>
-          </div>
-        </div>
-        <div className="border-t border-lgray py-4">
-          <b className="font-bold text-2xl">Descripción:</b>
-          <p>
-            {producto.descripcionModelo ||
-              'Este producto no tiene descripción aún.'}
-          </p>
-        </div>
-        <div>
-          <div className="w-full h-auto  flex justify-center items-center bg-white  border-y border-lgray">
-            <h2 className="py-2  font-black text-2xl">Preguntas</h2>
-          </div>
-
-          <p className="py-2 font-secondary text-xl">¿Qué quieres saber?</p>
-
-          <div className="flex flex-row gap-4">
-            <Button
-              className="bg-slate-100 border-primary text-primary hover:bg-slate-50"
-              onClick={() =>
-                setDefaultQuestion('¿Cuál es la garantía del producto?')
-              }
-            >
-              Garantía
-            </Button>
-            <Button
-              className="bg-slate-100 border-primary text-primary hover:bg-slate-50"
-              onClick={() =>
-                setDefaultQuestion('¿Cómo funcionan las devoluciones gratis?')
-              }
-            >
-              Devoluciones gratis
-            </Button>
-            <Button
-              className="bg-slate-100 border-primary text-primary hover:bg-slate-50"
-              onClick={() => setDefaultQuestion('¿El precio es negociable?')}
-            >
-              Precio
-            </Button>
-          </div>
-
-          <p className="py-2 pt-5 font-secondary text-xl">
-            Pregúntale al vendedor
-          </p>
-
-          <div className="flex flex-row gap-4">
-            <form
-              onSubmit={handleQuestion}
-              className="flex flex-col w-full max-w-4xl gap-3"
-            >
-              <div className="flex flex-row gap-2 justify-start">
-                <textarea
-                  id="pregunta"
-                  placeholder="Escribe aquí tu pregunta"
-                  rows="1"
-                  maxLength="45"
-                  className="mt-1 block w-full p-2 border border-blue-500 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              <div className="flex items-center flex-row">
+                <Input
+                  type="number"
+                  label="Cantidad"
+                  id="cantidad"
+                  name="cantidad"
+                  value={cantidad}
+                  onChange={(e) => setCantidad(Number(e.target.value))}
+                  min="1"
+                  max={producto.cantidad}
+                  className="mt-1 my-4 block w-1/3"
                 />
-                <Button
-                  type="submit"
-                  className="text-center bg-blue-500 text-white py-2 px-7  rounded-full"
-                >
-                  Preguntar
+                <Button className="mx-3" onClick={handleAddToCart}>
+                  🛒+{' '}
+                </Button>
+                <Button className="mx-3" onClick={handleBuy}>
+                  Comprar
                 </Button>
               </div>
-            </form>
+            </div>
           </div>
+          <div className="border-t border-lgray py-4">
+            <b className="font-bold text-2xl">Descripción:</b>
+            <p>
+              {producto.descripcionModelo ||
+                'Este producto no tiene descripción aún.'}
+            </p>
+          </div>
+          <div>
+            <div className="w-full h-auto  flex justify-center items-center bg-white  border-y border-lgray">
+              <h2 className="py-2  font-black text-2xl  ">Preguntas</h2>
+            </div>
 
-          <p className="py-2 pt-5 font-secondary text-xl">
-            {' '}
-            Últimas realizadas{' '}
-          </p>
+            <p className="py-2 font-secondary text-xl font-bold mt-4">
+              ¿Qué quieres saber?
+            </p>
 
-          <div className="flex flex-col gap-4">
-            {preguntas.length > 0 ? (
-              preguntas.map((pregunta, index) => (
-                <div key={index} className="flex flex-col gap-2">
-                  <p className="font-bold from-neutral-400">
-                    {pregunta.descripcion}
-                  </p>
-                  <p>{pregunta.respuesta}</p>
+            <div className="flex flex-row gap-4">
+              <Button
+                className="bg-slate-100 border-primary text-primary hover:bg-slate-50"
+                onClick={() =>
+                  setDefaultQuestion('¿Cuál es la garantía del producto?')
+                }
+              >
+                Garantía
+              </Button>
+              <Button
+                className="bg-slate-100 border-primary text-primary hover:bg-slate-50"
+                onClick={() =>
+                  setDefaultQuestion('¿Cómo funcionan las devoluciones gratis?')
+                }
+              >
+                Devoluciones gratis
+              </Button>
+              <Button
+                className="bg-slate-100 border-primary text-primary hover:bg-slate-50"
+                onClick={() => setDefaultQuestion('¿El precio es negociable?')}
+              >
+                Precio
+              </Button>
+            </div>
+
+            <p className=" my-4 font-bold text-xl ">Pregúntale al vendedor</p>
+
+            <div className="flex flex-row gap-4">
+              <form
+                onSubmit={handleQuestion}
+                className="flex flex-col w-full max-w-4xl gap-3"
+              >
+                <div className="flex flex-row gap-2 justify-start">
+                  <textarea
+                    id="pregunta"
+                    placeholder="Escribe aquí tu pregunta"
+                    rows="1"
+                    maxLength="45"
+                    className=" block w-full p-2 border border-primary rounded-md shadow-sm focus:ring-primary focus:border-primary sm:text-sm"
+                  />
+                  <Button
+                    type="submit"
+                    className="text-center bg-primary text-white py-2 px-7  rounded-xl h-full"
+                  >
+                    Preguntar
+                  </Button>
                 </div>
-              ))
-            ) : (
-              <p>No hay preguntas aún</p>
-            )}
-          </div>
+              </form>
+            </div>
 
-          <ProductRating />
+            <p className="py-2 pt-5 font-bold text-xl"> Últimas realizadas </p>
+
+            <div className="flex flex-col gap-4">
+              {preguntas.length > 0 ? (
+                preguntas.map((pregunta, index) => (
+                  <div key={index} className="flex flex-col gap-2">
+                    <p className="font-bold text-primary">
+                      {pregunta.descripcion}
+                    </p>
+                    <p>{pregunta.respuesta}</p>
+                  </div>
+                ))
+              ) : (
+                <p>No hay preguntas aún</p>
+              )}
+            </div>
+
+            <ProductRating />
+          </div>
         </div>
       </div>
     </section>
