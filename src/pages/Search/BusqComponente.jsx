0097ae6a6433efe70c666d895bd8d21ac47e6e3e
Homeroso <82355484+Homeroso@users.noreply.github.com>
2025-03-01 03:25:15 -0500

    Update component view

--- src/pages/Search/BusqComponente.jsx
+++ src/pages/Search/BusqComponente.jsx
@@ -50,95 +50,95 @@ const BicycleComponentFinder = () => {
         <h1 className="text-3xl font-bold text-center pt-8 mt-8">Encuentra los componentes para tu bicicleta</h1>
         <div className="flex flex-col md:flex-row p-2 w-full max-w-6xl mx-auto">
             
-        {/* Panel de imagen interactiva */}
-        <div className="w-full relative">
-            {/* Instrucciones */}
-            
-            <div className="bg-gray-100 rounded-lg p-2 h-full flex items-center justify-center">
+            {/* Panel de imagen interactiva */}
+            <div className="w-full relative">
+                {/* Instrucciones */}
                 
-            {/* Contenedor de la imagen y los hotspots */}
-            <div className="relative w-full max-w-lg mx-auto">
-                {/* Imagen de bicicleta */}
-                <img
-                src={bikeIcon}
-                alt="Bicicleta de montaña"
-                className="opacity-80 ">
-                </img>
-                {/* Áreas interactivas (hotspots) */}
-                <button 
-                className={`absolute bottom-40 left-16 w-20 h-20 rounded-full ${selectedComponent === 'wheels' ? 'bg-tertiary opacity-40' : 'bg-tertiary opacity-20 hover:opacity-40'}`}
-                onClick={() => handleComponentClick('Neumáticos')}
-                aria-label="Ruedas delanteras"
-                />
-                
-                <button 
-                className={`absolute bottom-40 right-16 w-20 h-20 rounded-full ${selectedComponent === 'wheels' ? 'bg-tertiary opacity-40' : 'bg-tertiary opacity-20 hover:opacity-40'}`}
-                onClick={() => handleComponentClick('Neumáticos')}
-                aria-label="Ruedas traseras"
-                />
+                <div className="bg-gray-100 rounded-lg p-2 h-full flex items-center justify-center">
+                    
+                {/* Contenedor de la imagen y los hotspots */}
+                    <div className="relative w-full max-w-lg mx-auto">
+                        {/* Imagen de bicicleta */}
+                        <img
+                        src={bikeIcon}
+                        alt="Bicicleta de montaña"
+                        className="opacity-80 ">
+                        </img>
+                        {/* Áreas interactivas (hotspots) */}
+                        <button 
+                        className={`absolute bottom-40 left-16 w-20 h-20 rounded-full ${selectedComponent === 'wheels' ? 'bg-tertiary opacity-40' : 'bg-tertiary opacity-20 hover:opacity-40'}`}
+                        onClick={() => handleComponentClick('Neumáticos')}
+                        aria-label="Ruedas delanteras"
+                        />
+                        
+                        <button 
+                        className={`absolute bottom-40 right-16 w-20 h-20 rounded-full ${selectedComponent === 'wheels' ? 'bg-tertiary opacity-40' : 'bg-tertiary opacity-20 hover:opacity-40'}`}
+                        onClick={() => handleComponentClick('Neumáticos')}
+                        aria-label="Ruedas traseras"
+                        />
 
-                <button 
-                className={`absolute bottom-64 right-28 w-12 h-12 rounded-full ${selectedComponent === 'wheels' ? 'bg-tertiary opacity-40' : 'bg-tertiary opacity-20 hover:opacity-40'}`}
-                onClick={() => handleComponentClick('Suspensión')}
-                aria-label="Ruedas delanteras"
-                />
-                
-                <button 
-                className={`absolute top-28 right-8 -translate-x-1/2 w-16 h-24 rounded-full ${selectedComponent === 'handlebars' ? 'bg-secondary opacity-40' : 'bg-secondary opacity-20 hover:opacity-40'}`}
-                onClick={() => handleComponentClick('Manubrios')}
-                aria-label="Manillar"
-                />
+                        <button 
+                        className={`absolute bottom-64 right-28 w-12 h-12 rounded-full ${selectedComponent === 'wheels' ? 'bg-tertiary opacity-40' : 'bg-tertiary opacity-20 hover:opacity-40'}`}
+                        onClick={() => handleComponentClick('Suspensión')}
+                        aria-label="Ruedas delanteras"
+                        />
+                        
+                        <button 
+                        className={`absolute top-28 right-8 -translate-x-1/2 w-16 h-24 rounded-full ${selectedComponent === 'handlebars' ? 'bg-secondary opacity-40' : 'bg-secondary opacity-20 hover:opacity-40'}`}
+                        onClick={() => handleComponentClick('Manubrios')}
+                        aria-label="Manillar"
+                        />
 
-                <button 
-                className={`absolute top-28 right-28 -translate-x-1/2 w-12 h-12 rounded-full ${selectedComponent === 'handlebars' ? 'bg-secondary opacity-40' : 'bg-secondary opacity-20 hover:opacity-40'}`}
-                onClick={() => handleComponentClick('Frenos')}
-                aria-label="Manillar"
-                />
-                
-                <button 
-                className={`absolute top-24 left-24 translate-x-4 w-32 h-12 rounded-full ${selectedComponent === 'saddle' ? 'bg-tertiary opacity-40' : 'bg-tertiary opacity-20 hover:opacity-40'}`}
-                onClick={() => handleComponentClick('Sillines')}
-                aria-label="Sillín"
-                />
-                
-                <button 
-                className={`absolute bottom-32 left-60 -translate-x-8 w-12 h-8 rounded-full ${selectedComponent === 'pedals' ? 'bg-tertiary opacity-40' : 'bg-tertiary opacity-20 hover:opacity-40'}`}
-                onClick={() => handleComponentClick('Pedales')}
-                aria-label="Pedales"
-                />
+                        <button 
+                        className={`absolute top-28 right-28 -translate-x-1/2 w-12 h-12 rounded-full ${selectedComponent === 'handlebars' ? 'bg-secondary opacity-40' : 'bg-secondary opacity-20 hover:opacity-40'}`}
+                        onClick={() => handleComponentClick('Frenos')}
+                        aria-label="Manillar"
+                        />
+                        
+                        <button 
+                        className={`absolute top-24 left-24 translate-x-4 w-32 h-12 rounded-full ${selectedComponent === 'saddle' ? 'bg-tertiary opacity-40' : 'bg-tertiary opacity-20 hover:opacity-40'}`}
+                        onClick={() => handleComponentClick('Sillines')}
+                        aria-label="Sillín"
+                        />
+                        
+                        <button 
+                        className={`absolute bottom-32 left-60 -translate-x-8 w-12 h-8 rounded-full ${selectedComponent === 'pedals' ? 'bg-tertiary opacity-40' : 'bg-tertiary opacity-20 hover:opacity-40'}`}
+                        onClick={() => handleComponentClick('Pedales')}
+                        aria-label="Pedales"
+                        />
 
-                <button 
-                className={`absolute bottom-44 left-60 -translate-x-8 w-12 h-12 rounded-full ${selectedComponent === 'pedals' ? 'bg-secondary opacity-40' : 'bg-secondary opacity-20 hover:opacity-40'}`}
-                onClick={() => handleComponentClick('Transmisión')}
-                aria-label="Pedales"
-                />
+                        <button 
+                        className={`absolute bottom-44 left-60 -translate-x-8 w-12 h-12 rounded-full ${selectedComponent === 'pedals' ? 'bg-secondary opacity-40' : 'bg-secondary opacity-20 hover:opacity-40'}`}
+                        onClick={() => handleComponentClick('Transmisión')}
+                        aria-label="Pedales"
+                        />
+                        
+                        {/* <button 
+                        className={`absolute top-32 right-40 w-12 h-12 rounded-full ${selectedComponent === 'brakes' ? 'bg-purple-500 opacity-40' : 'bg-purple-300 opacity-20 hover:opacity-40'}`}
+                        onClick={() => handleComponentClick('brakes')}
+                        aria-label="Frenos"
+                        /> */}
+                    </div>
+                </div>
                 
-                {/* <button 
-                className={`absolute top-32 right-40 w-12 h-12 rounded-full ${selectedComponent === 'brakes' ? 'bg-purple-500 opacity-40' : 'bg-purple-300 opacity-20 hover:opacity-40'}`}
-                onClick={() => handleComponentClick('brakes')}
-                aria-label="Frenos"
-                /> */}
             </div>
-            </div>
-            
-        </div>
-        </div>
         
-        {/* Panel de productos */}
-        <div className="w-full bg-white rounded-lg shadow p-2 h-full">
-            {selectedComponent ? (
-            <>
-                <IndividualProduct 
-                    products={filteredComponentes}
-                    title = {selectedComponent}/>
-            </>
-            ) : (
-            <div className="h-full flex items-center justify-center text-gray-500">
-                <p className="text-center text-bold text-xl">
-                Selecciona un componente de la bicicleta para ver los productos disponibles
-                </p>
+            {/* Panel de productos */}
+            <div className="w-1/2 bg-white rounded-lg shadow p-2 h-full">
+                {selectedComponent ? (
+                <>
+                    <IndividualProduct 
+                        products={filteredComponentes}
+                        title = {selectedComponent}/>
+                </>
+                ) : (
+                <div className="h-full flex items-center justify-center text-gray-500">
+                    <p className="text-center text-bold text-xl">
+                    Selecciona un componente de la bicicleta para ver los productos disponibles
+                    </p>
+                </div>
+                )}
             </div>
-            )}
         </div>
     </>
   );
