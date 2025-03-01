73323b4040bf0ed6762534733e70e9c51cffdf7a
JuMad-SE <jumadridc@unal.edu.co>
2025-03-01 16:15:32 -0500

    Modifica la detalles de redacción y definición de cada uno de los grados

--- src/components/Exposure/GradeInformation.jsx
+++ src/components/Exposure/GradeInformation.jsx
@@ -1,10 +1,10 @@
 const GradeInformation = ({ grade }) => {
   const gradeDescription = [
-    'Es el grado por defecto de las publicaciones realizadas en CicloMart para vendedores que no se encuentran asociados a ningún plan de pago. Estas publicaciones aparecen por detrás de productos con los demás grados de exposición tanto en búsquedas como en las secciones de su publicación.',
-    'Las publicaciones con este grado de exposición estarán por encima de aquellas con grado 0 tanto en búsquedas como en las secciones de su publicación y estarán  por debajo de aquellas con  grados 2, 3 y 4. ',
-    'Las publicaciones con este grado de exposición  tendrán prioridad sobre aquellas de grado 1 y 0 y estarán por debajo de las de grado 3 y 4. ',
-    'Las publicaciones con este grado de exposición tendrán prioridad sobre aquellas de grado 0,1 y 2 tanto en su aparición en las secciones principales como en las búsquedas y estarán por debajo de las de grado  4.',
-    'Ofrece la máxima exposición en su sección asociada. Estas publicaciones aparecerán en la sección de "Recomendados" en la página de inicio y aparecerán por encima de los demás productos en la sección de búsqueda.',
+    'Es el grado por defecto de las publicaciones realizadas en CicloMart para vendedores que no se encuentran asociados a ningún plan de pago. Estas publicaciones aparecen por detrás de productos con los demás grados de exposición tanto en búsquedas, filtros como así como en la sección de su publicación.',
+    'Las publicaciones con este grado de exposición estarán por encima de aquellas con (grado 0)  tanto en búsquedas como en las sección de su publicación y estarán  por debajo de aquellas con  (grados 2, 3 y 4) . ',
+    'Las publicaciones con este grado de exposición  tendrán prioridad sobre aquellas de (grado 0 y 1) y estarán por debajo de las de (grado 3 y 4). ',
+    'Las publicaciones con este grado de exposición tendrán prioridad sobre aquellas de (grado 0,1 y 2) tanto en su aparición en las secciones principales como en las búsquedas y estarán por debajo de las de (grado  4).',
+    'Ofrece la máxima exposición en su sección asociada estando por encima de todas las publicaciones con un nivel menor de exposición  ',
   ]
 
   return (
