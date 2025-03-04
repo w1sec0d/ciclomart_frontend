6812170ede95741c3dd5519b66627cf0175c7f22
JuMad-SE <jumadridc@unal.edu.co>
2025-03-04 00:31:39 -0500

    Añade menu a barra lateral

--- src/components/Profile/PrincipalSections/SideBar.jsx
+++ src/components/Profile/PrincipalSections/SideBar.jsx
@@ -7,6 +7,7 @@ import ShoppingBag from '@mui/icons-material/LocalMallOutlined'
 import Store from '@mui/icons-material/StorefrontOutlined'
 import Tag from '@mui/icons-material/LocalOfferOutlined'
 import ReviewsIcon from '@mui/icons-material/Reviews'
+import MenuIcon from '@mui/icons-material/Menu' // Importa el icono de menú
 
 //-> Images
 import Logo from '../../../assets/logoVector.svg'
@@ -26,6 +27,7 @@ const SideBar = () => {
   const [purchaseData, setPurchaseData] = useState([])
   const [salesData, setSalesData] = useState([])
   const [storesData, setStoresData] = useState([])
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false) // Estado para controlar la visibilidad del sidebar
   /*Cambia de pegina a mis reseñas */
   const navigate = useNavigate()
 
@@ -65,43 +67,66 @@ const SideBar = () => {
     navigate(`/vendedor/${authUser.idUsuario}`)
   }
 
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen)
+  }
+
   const purchasesUrl = authUser
     ? `/purchases/${authUser.idUsuario}`
     : '/purchases'
 
   /* -> Contenido Visual -> */
   return (
-    <div className="bg-lgray w-[20%] h-full shadow-2xl flex flex-col">
-      <ul>
-        <li>
+    <div
+      className={`fixed top-[64px] left-0 bg-lgray h-[calc(100%-64px)] shadow-2xl flex flex-col ${
+        isSidebarOpen ? 'w-[20%]' : 'w-0'
+      } transition-width duration-300 z-30`}
+    >
+      <button
+        onClick={toggleSidebar}
+        className={`absolute top-4 transition-left duration-300 ${
+          isSidebarOpen ? 'left-[100%]' : 'left-4'
+        } z-40`}
+      >
+        <MenuIcon />
+      </button>
+      <ul
+        className={`flex flex-col items-center ${isSidebarOpen ? 'block' : 'hidden'}`}
+      >
+        <li className="w-full">
           <Link to={purchasesUrl}>
-            <CardButton arrow={null}>
+            <CardButton
+              arrow={null}
+              className="flex items-center justify-center"
+            >
               <ShoppingBag className="ml-2" />
-              <b className="flex flex-col w-full">Compras</b>
+              <b className="flex flex-col w-full text-center">Compras</b>
             </CardButton>
           </Link>
           <hr />
         </li>
-        <li>
+        <li className="w-full">
           <CardButton
             onClick={() => {
               handleShowData(2)
             }}
+            className="flex items-center justify-center"
           >
             <Tag className="ml-2" />
-            <b className="flex flex-col w-full">Ventas</b>
+            <b className="flex flex-col w-full text-center">Ventas</b>
           </CardButton>
           <hr />
           <ShowDataList data={salesData} type={2} activeButton={activeButton} />
         </li>
-        <li>
+        <li className="w-full">
           <CardButton
             onClick={() => {
               handleShowData(3)
             }}
+            className="flex items-center justify-center"
           >
             <Store className="ml-2" />
-            <b className="flex flex-col w-full">Tiendas</b>
+            <b className="flex flex-col w-full text-center">Tiendas</b>
           </CardButton>
           <hr />
           <ShowDataList
@@ -110,15 +135,21 @@ const SideBar = () => {
             activeButton={activeButton}
           />
         </li>
-        <li>
-          <CardButton onClick={handleRedirect} arrow={0}>
+        <li className="w-full">
+          <CardButton
+            onClick={handleRedirect}
+            arrow={0}
+            className="flex items-center justify-center"
+          >
             <ReviewsIcon className="ml-2" />
-            <b className="flex flex-col w-full">Mis Reseñas</b>
+            <b className="flex flex-col w-full text-center">Mis Reseñas</b>
           </CardButton>
         </li>
         <hr />
       </ul>
-      <div className="h-full flex justify-center">
+      <div
+        className={`h-full flex justify-center ${isSidebarOpen ? 'block' : 'hidden'}`}
+      >
         <img
           src={Logo}
           className="w-[160px] h-[140px] mt-8 px-1 opacity-60 grayscale"
