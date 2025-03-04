c46f8cebb484e22156a5655f897092aec44ccf09
Carlos Ramírez <cramirezmun@unal.edu.co>
2025-03-03 23:14:32 -0500

    Ajusta estilos en el footer para mejorar la presentación y la alineación de los elementos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = forwardRef((props, ref) => {
     <footer ref={ref} className="w-full bg-primary">
       <div className="flex flex-col h-full w-full">
         <div className="container mx-auto py-4 flex flex-col md:flex-row pl-6 text-white">
-          <ul className="w-full md:w-1/3 border-b md:border-b-0 md:border-r border-primaryDark mb-4 md:mb-0">
+          <ul className="w-full md:w-1/3 border-b md:border-b-0 md:border-r border-primaryDark md:mb-0 py-6 md:py-0">
             <h3 className="text-xl font-bold mb-4">Legalidad</h3>
             <li>
               <Link to="/terms" className="hover:text-secondary">
@@ -20,26 +20,26 @@ const Footer = forwardRef((props, ref) => {
               </Link>
             </li>
           </ul>
-          <ul className="w-full md:w-1/3 md:ml-4 border-b md:border-b-0 md:border-r border-primaryDark mb-4 md:mb-0">
+          <ul className="w-full md:w-1/3 md:ml-4 border-b md:border-b-0 md:border-r border-primaryDark mb-4 md:mb-0 py-6 md:py-0">
             <h3 className="text-xl font-bold mb-4">Recursos</h3>
-            <li className="flex items-center">
-              <div className="flex items-center">
+            <li className="flex items-center flex-col justify-center">
+              <div className="flex items-center w-full">
                 <Link
                   to="https://github.com/w1sec0d/CicloMart_Back"
                   target="_blanck"
                 >
-                  <div className="hover:text-secondary flex flex-row">
+                  <div className="hover:text-secondary flex flex-row justify-center items-center">
                     <p className="mr-2">Github BackEnd</p>
                     <FaGithub className="text-3xl" />
                   </div>
                 </Link>
               </div>
-              <div className="flex items-center">
+              <div className="flex items-center w-full mt-6">
                 <Link
                   to="https://github.com/w1sec0d/CicloMart_front"
                   target="_blanck"
                 >
-                  <div className="hover:text-secondary flex flex-row">
+                  <div className="hover:text-secondary flex flex-row justify-center items-center">
                     <p className="mr-2">Github FrontEnd</p>
                     <FaGithub className="text-3xl" />
                   </div>
@@ -47,7 +47,7 @@ const Footer = forwardRef((props, ref) => {
               </div>
             </li>
           </ul>
-          <ul className="w-full md:w-1/3 md:ml-4">
+          <ul className="w-full md:w-1/3 md:ml-4 py-6 md:py-0">
             <h3 className="text-xl font-bold mb-4">Responsables</h3>
             <li>
               <Link
