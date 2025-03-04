e9db53838e0cebed8d5904d94f20d8f753f7b3a0
JuMad-SE <jumadridc@unal.edu.co>
2025-03-03 21:17:41 -0500

    Ajusta estilos del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,35 +10,81 @@ const Footer = forwardRef((props, ref) => {
           <ul className="w-1/3 border-r border-primaryDark ">
             <h3 className="text-xl font-bold mb-4">Legalidad</h3>
             <li>
-              <Link to="/terms">Terminos y condiciones</Link>
+              <Link to="/terms" className="hover:text-secondary">
+                Terminos y condiciones
+              </Link>
             </li>
             <li>
-              <Link to="/privacy">Politica de datos</Link>
+              <Link to="/privacy" className="hover:text-secondary">
+                Politica de datos
+              </Link>
             </li>
           </ul>
           <ul className="w-1/3 ml-4 border-r border-primaryDark ">
             <h3 className="text-xl font-bold mb-4">Recursos</h3>
             <li className="flex flex-col space-y-2">
-              <div className='flex items-center'>
-                <p className="mr-2">Github BackEnd</p>
-                <Link to='https://github.com/w1sec0d/CicloMart_Back'>
-                  <FaGithub className="text-3xl" />
+              <div className="flex items-center">
+                <Link
+                  to="https://github.com/w1sec0d/CicloMart_Back"
+                  target="_blanck"
+                >
+                  <div className="hover:text-secondary flex flex-row">
+                    <p className="mr-2">Github BackEnd</p>
+                    <FaGithub className="text-3xl" />
+                  </div>
                 </Link>
               </div>
-              <div className='flex items-center'>
-                <p className="mr-2">Github FrontEnd</p>
-                <Link to='https://github.com/w1sec0d/CicloMart_front'>
-                  <FaGithub className="text-3xl" />
+              <div className="flex items-center">
+                <Link
+                  to="https://github.com/w1sec0d/CicloMart_front"
+                  target="_blanck"
+                >
+                  <div className="hover:text-secondary flex flex-row">
+                    <p className="mr-2">Github FrontEnd</p>
+                    <FaGithub className="text-3xl" />
+                  </div>
                 </Link>
               </div>
             </li>
           </ul>
           <ul className="w-1/3 ml-4 ">
             <h3 className="text-xl font-bold mb-4">Responsables</h3>
-            <li>Carlos David Ramírez Muñoz</li>
-            <li>Johan David Rodríguez Gutíerrez</li>
-            <li>Ronald Daniel Jacanamejoy Mutumbajoy</li>
-            <li>Juan David Madrid Contreras</li>
+            <li>
+              <Link
+                to={'https://github.com/w1sec0d'}
+                className="hover:text-secondary"
+                target="_blanck"
+              >
+                Carlos David Ramírez Muñoz
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={'https://github.com/Homeroso'}
+                className="hover:text-secondary"
+                target="_blanck"
+              >
+                Johan David Rodríguez Gutíerrez
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={'https://github.com/RonaldDaniel20'}
+                className="hover:text-secondary"
+                target="_blanck"
+              >
+                Ronald Daniel Jacanamejoy Mutumbajoy
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={'https://github.com/JuMad-SE'}
+                className="hover:text-secondary"
+                target="_blanck"
+              >
+                Juan David Madrid Contreras
+              </Link>
+            </li>
           </ul>
         </div>
         <div className="py-4 w-full border-t border-primaryDark flex flex-row px-6">
